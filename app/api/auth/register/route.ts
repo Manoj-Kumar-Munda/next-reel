98500d1d954991410a2d49a0083cb013aa4f4d5e
Manoj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-06-21 21:13:22 +0530

    feat: implement user registration API and update dependencies

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import { User } from "@/models/User";
+import { connectToDatabase } from "@/lib/db";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        {
+          error: "Email and password are required",
+        },
+        { status: 400 }
+      );
+    }
+    await connectToDatabase();
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          error: "User already registered",
+        },
+        { status: 400 }
+      );
+    }
+
+    const newUser = await User.create({ email, password });
+
+    return NextResponse.json(
+      { message: "User registered successfully", newUser },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "User registration failed" },
+      { status: 400 }
+    );
+  }
+}
